f2a7be95dcac892394cb9faa1465f9aecb7a887c
Alexander McKinley <alexanderfromaustralia@gmail.com>
2025-05-24 11:57:00 +0900

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,40 @@
-const { src, dest, series, parallel } = require('gulp');
-const htmlmin = require('gulp-htmlmin');
-const cleanCSS = require('gulp-clean-css');
-const terser = require('gulp-terser');
-const imagemin = require('gulp-imagemin').default;
-const mozjpeg = require('imagemin-mozjpeg');
-const optipng = require('imagemin-optipng');
-const del = require('del');
+const { src, dest, series, parallel } = require("gulp");
+const htmlmin = require("gulp-htmlmin");
+const cleanCSS = require("gulp-clean-css");
+const terser = require("gulp-terser");
+const del = require("del");
 
 function clean() {
-  return del(['output']);
+  return del(["output"]);
 }
 
 function minifyHTML() {
-  return src('src/*.html')
+  return src("src/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest('output'));
+    .pipe(dest("output"));
 }
 
 function minifyCSS() {
-  return src('src/*.css')
-    .pipe(cleanCSS())
-    .pipe(dest('output'));
+  return src("src/*.css").pipe(cleanCSS()).pipe(dest("output"));
 }
 
 function minifyJS() {
-  return src('src/*.js')
-    .pipe(terser())
-    .pipe(dest('output'));
+  return src("src/*.js").pipe(terser()).pipe(dest("output"));
 }
 
-function optimizeImages() {
-  return src('src/img/**/*.{jpg,png}')
-    .pipe(imagemin([
-      mozjpeg({ quality: 75, progressive: true }),
-      optipng({ optimizationLevel: 5 })
-    ]))
-    .pipe(dest('output/img'));
+async function optimizeImages() {
+  const imagemin = (await import("gulp-imagemin")).default;
+  const mozjpeg = (await import("imagemin-mozjpeg")).default;
+  const optipng = (await import("imagemin-optipng")).default;
+
+  return src("src/img/**/*.{jpg,png}")
+    .pipe(
+      imagemin([
+        mozjpeg({ quality: 75, progressive: true }),
+        optipng({ optimizationLevel: 5 }),
+      ])
+    )
+    .pipe(dest("output/img"));
 }
 
 exports.default = series(
